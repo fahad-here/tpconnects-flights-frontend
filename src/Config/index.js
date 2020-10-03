1f4108a7f11e3ecc9caafc9fd83fab2ef3e838b0
fahad_laptop <fmohajir@gmail.com>
2020-10-03 12:30:29 +0400

    Added API config file

---
+++ src/Config/index.js
@@ -0,0 +1,6 @@
+const DEV_LOCAL_URL = 'http://localhost:$PORT'
+const PRODUCTION_URL = 'http://localhost:$PORT'
+const BASE_URL = DEV_LOCAL_URL
+const API_PORT = '3000'
+
+export { BASE_URL, API_PORT }
